21c3382e08d354067e28b5e1014bd7b430a206e2
jx11r <jx11r@hotmail.com>
2022-07-17 23:51:25 -0500

    src: add env.py

---
+++ src/env.py
@@ -0,0 +1,23 @@
+import os, subprocess
+from src.bridge import CONFIG
+
+def apply() -> None:
+  envsubst = './.apt/usr/bin/envsubst'
+  subprocess.Popen(
+    [f'{envsubst} < file/config.toml > {CONFIG}'],
+    shell = True,
+  ).wait()
+
+token = {
+  'discord': os.environ.get('DISCORD_TOKEN'),
+  'github': os.environ.get('GITHUB_TOKEN'),
+  'telegram': os.environ.get('TELEGRAM_TOKEN'),
+}
+
+webhook = {
+  'issues': os.environ.get('WEBHOOK_ISSUES'),
+  'logs': os.environ.get('WEBHOOK_LOGS'),
+  'reddit': os.environ.get('WEBHOOK_REDDIT'),
+  'releases': os.environ.get('WEBHOOK_RELEASES'),
+  'tests': os.environ.get('WEBHOOK_TESTS'),
+}
