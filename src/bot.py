0fde81c9fe6cc3ebdc952cfcf857d981e6d88872
jx11r <jx11r@hotmail.com>
2022-07-19 16:42:00 -0500

    bot: update commands

--- src/bot.py
+++ src/bot.py
@@ -1,7 +1,7 @@
-import discord, traceback, time
+import discord, traceback, time, json
 from discord.ext import commands
 
-from src import env, bridge
+from src import env, bridge, test, webhooks
 from src.utils import logger
 
 active = discord.Activity(
@@ -21,43 +21,100 @@ bot = commands.Bot(
   status = discord.Status.online,
 )
 
+def Embed(description):
+  return discord.Embed(
+    # color = 0xffffff,
+    description = description,
+  )
+
 @bot.command()
 async def status(context):
   if bridge.status():
-    await context.send('Bridge: active.')
+    await context.send(embed = Embed(':green_circle: Active'))
   else:
-    await context.send('Bridge: inactive.')
+    await context.send(embed = Embed(':red_circle: Inactive'))
 
 @bot.command()
 async def version(context):
-  await context.send(bridge.version())
+  await context.send(embed = Embed(bridge.version()))
 
 @bot.command()
 @commands.has_any_role('admin', 'dev')
 async def start(context):
-  await context.send(bridge.start())
+  await context.send(embed = Embed(bridge.start()))
   await bot.change_presence(activity = active)
 
 @bot.command()
 @commands.has_any_role('admin', 'dev')
 async def stop(context):
-  await context.send(bridge.stop())
+  await context.send(embed = Embed(bridge.stop()))
   await bot.change_presence(activity = inactive)
 
 @bot.command()
 @commands.has_any_role('admin', 'dev')
 async def restart(context):
-  await context.send(bridge.restart())
+  await context.send(embed = Embed(bridge.restart()))
   await bot.change_presence(activity = active)
+
+msg = 'Error: No matches found.\nPossible values: issue, reddit or release.'
+
+@bot.command()
+@commands.has_any_role('admin', 'dev')
+async def payload(context, name):
+  webhook = {
+    'issue': webhooks.issues,
+    'reddit': webhooks.reddit,
+    'release': webhooks.releases,
+  }.get(name, False)
+
+  if webhook:
+    try:
+      await context.send(
+        f'```json\n{json.dumps(webhook.payload, indent = 2)}```'
+      )
+    except:
+      await context.send(embed = Embed(webhook.payload))
+  else:
+    await context.send(embed = Embed(msg))
+
+@bot.command()
+@commands.has_any_role('admin', 'dev')
+async def resend(context, name):
+  webhook = {
+    'issue': webhooks.issues,
+    'reddit': webhooks.reddit,
+    'release': webhooks.releases,
+  }.get(name, False)
+
+  if webhook:
+    webhook.send()
+  else:
+    await context.send(embed = Embed(msg))
+
+@bot.command()
+@commands.has_any_role('admin', 'dev')
+async def send(context, name):
+  webhook = {
+    'issue': test.issues,
+    'reddit': test.reddit,
+    'release': test.releases,
+  }.get(name, False)
+
+  if webhook:
+    webhook.send()
+  else:
+    await context.send(embed = Embed(msg))
 
 @bot.event
 async def on_command_error(context, error):
   if isinstance(error, commands.CommandNotFound):
     pass
   elif isinstance(error, commands.MissingAnyRole):
-    await context.send("You don't have permission to use this command.")
+    await context.send(
+      embed = Embed("You don't have permission to use this command.")
+    )
   else:
-    print(''.join(
+    logger(''.join(
       traceback.format_exception(type(error), error, error.__traceback__)
     ))
 
