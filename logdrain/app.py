ae2e27724b7920afe96a07b21f38aad57ee7703f
jx11r <jx11r@hotmail.com>
2022-07-20 17:04:21 -0500

    logdrain: sanitize logs

--- logdrain/app.py
+++ logdrain/app.py
@@ -1,25 +1,29 @@
 from flask import Flask, request, jsonify
-import os, requests
+import os, requests, re
 
 app = Flask(__name__)
 webhook = os.environ.get('WEBHOOK_LOGS')
 
 @app.route('/logs', methods = ['POST'])
 async def logs():
-  log = request.data.decode('utf-8')
-  return jsonify({'status': send(log)})
+  sanitize(request.data.decode('utf-8'))
+  return jsonify({'status': 'ok'})
 
-def send(content) -> str:
+def sanitize(text: str) -> None:
+  for i in text.split('\n'):
+    send(re.sub('^.*\s\d{4}.+\w.*-\s', '', i.strip()))
+
+def send(text: str) -> str:
   payload = {
     'username': 'Heroku',
     'embeds': [{
       'color': 0x79589f,
-      'description': content,
+      'description': text,
     }]
   }
 
   with requests.post(webhook, json = payload) as response:
-    return str(response.status_code)
+    return response.status_code
 
 port = int(os.environ.get('PORT', 5000))
 app.run(debug = False, host = '0.0.0.0', port = port)
